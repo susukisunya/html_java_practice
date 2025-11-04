0d35d5baad9ed3ec265f1856af8577789f52be57
susukisunya <aynuj0358@icloud.com>
2025-11-04 23:18:22 +0900

    変数名を変更した

--- main.js
+++ main.js
@@ -2,13 +2,27 @@
 
 //クリックによって数値が変動するカウンターを作成する
 var plus1_number = () => {
-  var countnumber = document.querySelector(".edit_area_number");//getElementsByClassNameではうまくいかなかった　なんで？
-  var intcountnumber = Number(countnumber.textContent);
-  intcountnumber++;
-  console.log(intcountnumber);
-  countnumber.textContent = intcountnumber;
+  var counternumber = document.querySelector(".edit_area_number");//getElementsByClassNameではうまくいかなかった　なんで？
+  var intcounternumber = Number(counternumber.textContent);
+  intcounternumber++;
+  console.log(intcounternumber);
+  counternumber.textContent = intcounternumber;
 }
 
 var be0_number = () => {
-    document.querySelector(".edit_area_number").textContent = 0;
+  var counternumber = document.querySelector(".edit_area_number");
+  counternumber.textContent = 0;
+}
+
+//秒数を表示する時計を作る
+var clock = () => {
+  var date = new Date();
+  var clock = date.getFullYear()
+  + '/' + ('0' + (date.getMonth() + 1)).slice(-2)
+  + '/' + ('0' + date.getDate()).slice(-2)
+  + ' ' + ('0' + date.getHours()).slice(-2)
+  + ':' + ('0' + date.getMinutes()).slice(-2)
+  + ':' + ('0' + date.getSeconds()).slice(-2)
+  + '(JST)';
+  document.getElementsByClassName("clock").textContent = clock;
 }
